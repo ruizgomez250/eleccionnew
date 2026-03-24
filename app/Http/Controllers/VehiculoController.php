d8583cb99b99fcd54eff562a1a49bdf369f463c2
ruizgomez250 <ruizgomez250@gmail.com>
2026-03-24 11:19:29 -0300

    mensaje

--- app/Http/Controllers/VehiculoController.php
+++ app/Http/Controllers/VehiculoController.php
@@ -291,7 +291,6 @@ Este recibo constituye constancia de pago parcial o total conforme al contrato d
         $todos = Puntero::where('id_equipo', $equipoId)
             ->whereNotIn('id', $asignados->pluck('id'))
             ->get();
-        dd($todos);
         return response()->json([
             'asignados' => $asignados,
             'todos' => $todos
