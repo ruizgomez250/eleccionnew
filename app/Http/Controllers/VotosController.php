448ac554d6837b098ac03f3070a31287718a6590
ruizgomez250 <ruizgomez250@gmail.com>
2026-06-01 09:51:54 -0300

    mensaje

--- app/Http/Controllers/VotosController.php
+++ app/Http/Controllers/VotosController.php
@@ -22,11 +22,10 @@ class VotosController extends Controller
     {
         try {
             $cedula = base64_decode(strtr($cedula_encriptada, '-_', '+/'));
-            dd($cedula);
             if ($cedula === false) {
                 return view('cargar-votos-error', ['error' => 'No se puede abrir la página. El enlace no es válido.']);
             }
-
+            dd('paso');
             $miembro = MiembroDeMesa::where('cedula', $cedula)->with('equipo')->firstOrFail();
             $equipo = $miembro->equipo;
             $cantidadMesas = 12;
